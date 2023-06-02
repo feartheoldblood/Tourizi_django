c34702648217acd93eb7d7be51b181312055f50c
xd <zuotianyesterday@gmail.com>
2023-06-01 21:41:59 -0500

    sprint 1 avance

--- tourizi/urls.py
+++ tourizi/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for tourizi project.
+URL configuration for djangocrud project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -17,6 +17,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from tasks import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', views.home, name= 'home'),
+    path('signup/', views.signup, name ='signup' ),
+    path('tasks/', views.tasks, name ='tasks' ),
+    path('tasks_completed/', views.tasks_completed, name ='tasks_completed' ),
+    path('logout/', views.signout, name ='logout' ),
+    path('signin/', views.signin, name ='signin' ),
+    path('tasks/create/', views.create_task, name ='create_task' ),
+    path('tasks/<int:task_id>/', views.task_detail , name= 'task_detail'),
+    path('tasks/<int:task_id>/complete', views.complete_task, name= 'complete_task'),
+    path('tasks/<int:task_id>/delete', views.delete_task  , name= 'delete_task'),
+
+] 
+
+
